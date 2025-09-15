0c15b06d2c2c0d9fc6bd40482aa9a6bdb7beb883
연구개발전담부 <wdpb@mig.kr>
2025-09-15 11:45:33 +0900

    update: layout 구조 개편

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col`}>
         <TopButton />
         <CallBtn />
         <Gnb />
